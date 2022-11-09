b8dd9c91c8ae667a02dfed3e4574f33fff7dc970
shebl007byte <omar.mahmoud001@eng-st.cu.edu.eg>
2022-11-09 15:23:03 +0200

    add: downloadExcels.js, extractClassEntry.js
    Scripts for automating class's excels download operation

---
+++ Server/src/Scripts/downloadExcels.js
@@ -0,0 +1,88 @@
+const { data } = require("jquery");
+const jsdom = require("jsdom");
+const Downloader = require("nodejs-file-downloader");
+const {extract_room, extract_schid, extract_time_to24} = require("./extractClassEntry")
+
+
+async function get_classes_html() {
+    const res = await fetch('http://chreg.eng.cu.edu.eg/ClassList.aspx?s=1');
+    const html = await res.text();
+    return html
+}
+
+async function parse_html(string) {
+    const dom = new jsdom.JSDOM(string);
+    const document = dom.window.document;
+    table = document.getElementById("GridView1");
+    tbody = table.querySelector("tbody");
+    rows = tbody.querySelectorAll("tr");
+
+    const website = 'http://chreg.eng.cu.edu.eg/';
+    let data = [];
+
+    for(let i = 1; i < rows.length; i++) {
+        columns = rows[i].getElementsByTagName("td");
+        
+        course_code = columns[0].textContent;
+        course_name = columns[1].textContent;
+        class_room = columns[2].textContent;
+        session_type = columns[3].textContent;
+        day_name = columns[4].textContent;
+        begin_time = columns[5].textContent;
+        end_time = columns[6].textContent;
+        gp = columns[7].textContent;
+        lnk = (columns[8].getElementsByTagName("a"))[0].getAttribute("href");
+        
+        schoolid = extract_schid(lnk);
+        class_room =  extract_room(class_room);
+        begin_time = extract_time_to24(begin_time);
+        end_time = extract_time_to24(end_time);
+        lnk = website+lnk;
+
+        data.push({
+            index:   i,
+            schid:   schoolid,
+            course:  course_code,
+            name:    course_name,
+            room:    class_room,
+            type:    session_type,
+            day:     day_name,
+            begin:   begin_time,
+            end:     end_time,
+            group:   gp,
+            link:    lnk
+        });
+    }
+    return data;
+}
+
+async function dowloadLink(entry, directory){
+    const downloader = new Downloader({
+        url: entry.link,
+        directory: directory,
+        fileName: `${entry.index}_${entry.schid}_${entry.course}_${entry.type}.xlsx`,
+        maxAttempts: 5
+    });
+
+    try {
+        await downloader.download();
+    } catch (error) {
+        console.log(`Download failed for ${entry.link}`, error);
+    }
+}
+
+
+async function downloadExcels(directory) {
+    console.time("Excels Download Time")
+
+    const html = await get_classes_html();
+    const data = await parse_html(html);
+
+    for (const entry of data) {
+        await dowloadLink(entry, directory);
+    }
+
+    console.timeEnd("Excels Download Time")
+}
+
+module.exports = downloadExcels
---
+++ Server/src/Scripts/extractClassEntry.js
@@ -0,0 +1,59 @@
+function extract_room(class_room) {
+    if (!isNaN(class_room)) {
+        class_room = parseInt(class_room);
+    } else if (class_room[0] === '[') {
+        if (class_room[1] === '0') {
+            class_room = class_room.slice(3)
+            class_room = class_room.split("-");
+            if (!isNaN(class_room[0])) {
+                class_room = parseInt(class_room[0])
+            } else {
+                class_room = 16311; //TODO: Get Real Value (Lab ELC. Building)
+            }
+        } else {
+            class_room = class_room.split("-")[0];
+            class_room = class_room.split("]")[0];
+            class_room = parseInt(class_room.slice(1));
+        }
+    } else if (class_room[0] === 'ا') {
+        class_room = parseInt(class_room.split("-")[2].split("]")[0].split("[")[1])
+    } else if (class_room.split("-").length == 2) {
+        class_room = parseInt(class_room.split("-")[0])
+    } else if (class_room === "2nd floor lab1") {
+        class_room = 3201;    //TODO: Get Real Value
+    } else if (class_room === "2nd floor lab 2" || class_room === "2nd floor lab2") {
+        class_room = 3202;    //TODO: Get Real Value
+    } else if (class_room === "2nd floor lab3") {
+        class_room = 3203;    //TODO: Get Real Value
+    } else if (class_room === "LAB CHE") {
+        class_room = 322;     //TODO: Get Real Value
+    } else if (class_room.includes("+")) {
+        class_room = class_room.split("+")
+        class_room[0] = extract_room(class_room[0]);
+        class_room[1] = extract_room(class_room[1]);
+    } else if (class_room === "-----") {
+        class_room = 0;
+    } else {
+        throw new Error(`Error not found: ${class_room}`);
+    }
+    
+    return class_room
+}
+
+function extract_schid(schid) {
+    schid = schid.split("?");
+    schid = schid[1].split("&");
+    return schid[0].split("=")[1];
+}
+
+function extract_time_to24(time) {
+    time = time.split(":");
+    hour = parseInt(time[0]);
+    minute = parseInt(time[1]);
+
+    if (minute > 0) hour += 1;
+    if (hour < 8) hour += 12;
+    return hour;
+}
+
+module.exports = {extract_room, extract_schid, extract_time_to24}
