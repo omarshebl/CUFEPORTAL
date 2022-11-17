6db98ccace62844545188c68123dc61e78258c94
shebl007byte <omar.mahmoud001@eng-st.cu.edu.eg>
2022-11-17 12:35:02 +0200

    upd: insert_database.js
    added insert_student_reg function to insert the data into the StudentRegClasses table, removed connection.connect() and connection.end() from all functions

--- Server/src/Scripts/insert_database.js
+++ Server/src/Scripts/insert_database.js
@@ -27,56 +27,63 @@ function get_day_number(day) {
 function insert_classes(connection, classes, version) {
     insert_classes_query = 'INSERT INTO Classes (CLASS_ID, SCH_ID, COURSE_CODE, CLASS_ROOM, CLASS_TYPE_LEC, CLASS_DAY, CLASS_START, CLASS_END, CLASS_GROUP, CLASS_STATUS, CLASS_NUM_ENROLED, CLASS_M_NUM_ENROLED, CLASS_GGL_CR, VERSION_ID) ' + 
                                     'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-    connection.connect();
-    shift = 0;
     for (c of classes) {
-        console.log(c)
         if (isNaN(c.room)) {
-            insert_query = [(c.index + shift), parseInt(c.schid), c.course, c.room[0], (c.type === 'Lecture'?1:0), get_day_number(c.day), c.begin, c.end, parseInt(c.group), 0, 0, 0, 'NONE', version];
+            insert_query = [(c.index), parseInt(c.schid), c.course, c.room[0], (c.type === 'Lecture'?1:0), get_day_number(c.day), c.begin, c.end, parseInt(c.group), 0, 0, 0, 'NONE', version];
             connection.execute({sql: insert_classes_query, values: insert_query},function (error) {
                 if (error) throw error;
             })
-            shift += 1;
-            insert_query = [(c.index + shift), parseInt(c.schid), c.course, c.room[1], (c.type === 'Lecture'?1:0), get_day_number(c.day), c.begin, c.end, parseInt(c.group), 0, 0, 0, 'NONE', version];
+            insert_query[3] = c.room[1];
             connection.execute({sql: insert_classes_query, values: insert_query},function (error) {
                 if (error) throw error;
             })
         } else {
-            insert_query = [(c.index + shift), parseInt(c.schid), c.course, c.room, (c.type === 'Lecture'?1:0), get_day_number(c.day), c.begin, c.end, parseInt(c.group), 0, 0, 0, 'NONE', version];
+            insert_query = [(c.index), parseInt(c.schid), c.course, c.room, (c.type === 'Lecture'?1:0), get_day_number(c.day), c.begin, c.end, parseInt(c.group), 0, 0, 0, 'NONE', version];
             connection.execute({sql: insert_classes_query, values: insert_query},function (error) {
                 if (error) throw error;
             })
         }
         //TODO: implement class_status, class_num_enrolled
     }
-    connection.end();
 }
 
 function insert_courses(connection, courses) {
     insert_courses_query = 'INSERT IGNORE INTO Courses (COURSE_CODE, COURSE_NAME, LECTURE_DUR, TUTORIAL_DUR) ' + 
                                     'VALUES (?, ?, ?, ?) ';
-    connection.connect();
     for (c of courses) {
         insert_query = [c[0], c[1], c[3], c[4]];
         connection.execute({sql: insert_courses_query, values: insert_query},function (error) {
             if (error) throw error;
         })
     }
-    connection.end();
 }
 
 function insert_students(connection, students) {
     insert_students_query = 'INSERT INTO Students (STUDENT_ID, STUDENT_NAME_A, STUDENT_NAME_E) ' + 
                                     'VALUES (?, ?, ?) ' +
                                     'ON DUPLICATE KEY UPDATE STUDENT_NAME_A = ?, STUDENT_NAME_E = ?';
-    connection.connect();
     for (s of students) {
         insert_query = [s.id, s.nameArabic, s.nameEnglish, s.nameArabic, s.nameEnglish];
         connection.execute({sql: insert_students_query, values: insert_query},function (error) {
             if (error) throw error;
         })
     }
-    connection.end();                            
 }
 
-module.exports = {insert_classes, insert_courses, insert_students}
+function insert_students_reg(connection, students_reg, version) {
+    insert_students_query = 'INSERT INTO StudentRegClasses (STUDENT_ID, CLASS_ID, REG_DATE, VERSION_ID) ' + 
+                                    'VALUES (?, ?, ?, ?) ';
+    for (s of students_reg) {
+        insert_query = [s.id, s.class_id, s.reg_date, version];
+        connection.execute({sql: insert_students_query, values: insert_query},function (error) {
+            if (error) throw error;
+        })
+    }
+}
+
+module.exports = {
+    insert_classes, 
+    insert_courses, 
+    insert_students, 
+    insert_students_reg
+}
