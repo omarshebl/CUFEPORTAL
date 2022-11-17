3f4a6106a7394d24a9bdcd1ec652cc2ec51eacec
shebl007byte <omar.mahmoud001@eng-st.cu.edu.eg>
2022-11-17 07:41:03 +0200

    mod: insert_classes.js -> insert_database.js
    modified script name to be more inclusive of other database inserts, added insert_courses function

--- Server/src/Scripts/insert_database.js
+++ Server/src/Scripts/insert_database.js
@@ -23,10 +23,10 @@ function get_day_number(day) {
             break;        
     }
 }
+
 function insert_classes(connection, classes, version) {
     insert_classes_query = 'INSERT INTO Classes (CLASS_ID, SCH_ID, COURSE_CODE, CLASS_ROOM, CLASS_TYPE_LEC, CLASS_DAY, CLASS_START, CLASS_END, CLASS_GROUP, CLASS_STATUS, CLASS_NUM_ENROLED, CLASS_M_NUM_ENROLED, CLASS_GGL_CR, VERSION_ID) ' + 
                                     'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-    let insert_classes = [];
     connection.connect();
     shift = 0;
     for (c of classes) {
@@ -36,24 +36,37 @@ function insert_classes(connection, classes, version) {
             connection.execute({sql: insert_classes_query, values: insert_query},function (error) {
                 if (error) throw error;
             })
-            insert_classes.push(insert_query);
             shift += 1;
             insert_query = [(c.index + shift), parseInt(c.schid), c.course, c.room[1], (c.type === 'Lecture'?1:0), get_day_number(c.day), c.begin, c.end, parseInt(c.group), 0, 0, 0, 'NONE', version];
             connection.execute({sql: insert_classes_query, values: insert_query},function (error) {
                 if (error) throw error;
             })
-            insert_classes.push(insert_query);
         } else {
             insert_query = [(c.index + shift), parseInt(c.schid), c.course, c.room, (c.type === 'Lecture'?1:0), get_day_number(c.day), c.begin, c.end, parseInt(c.group), 0, 0, 0, 'NONE', version];
             connection.execute({sql: insert_classes_query, values: insert_query},function (error) {
                 if (error) throw error;
             })
-            insert_classes.push(insert_query);
         }
         //TODO: implement class_status, class_num_enrolled
     }
     connection.end();
-    return insert_classes;
 }
 
-module.exports = insert_classes
+function insert_courses(connection, courses) {
+    insert_courses_query = 'INSERT IGNORE INTO Courses (COURSE_CODE, COURSE_NAME, LECTURE_DUR, TUTORIAL_DUR) ' + 
+                                    'VALUES (?, ?, ?, ?) ';
+    connection.connect();
+    for (c of courses) {
+        insert_query = [c[0], c[1], c[3], c[4]];
+        connection.execute({sql: insert_courses_query, values: insert_query},function (error) {
+            if (error) throw error;
+        })
+    }
+    connection.end();
+}
+
+function insert_students(connection, students) {
+    
+}
+
+module.exports = {insert_classes, insert_courses}
