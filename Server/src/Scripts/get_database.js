835ada29f69938dd2b707477582a01d5caf2a948
shebl007byte <omar.mahmoud001@eng-st.cu.edu.eg>
2022-11-17 12:32:44 +0200

    mod: get_database.js
    removed connection.connect() and connection.end()

--- Server/src/Scripts/get_database.js
+++ Server/src/Scripts/get_database.js
@@ -1,11 +1,9 @@
 function get_last_version(connection, callback) {
     return new Promise((resolve, reject) => {
-        connection.connect();
         connection.execute("SELECT MAX(VERSION_ID) FROM StudentRegClassesVer", function(err, result) {
             if (err) reject(err);
             resolve(result[0]['MAX(VERSION_ID)']);
         })
-        connection.end();
     })
 }
 
