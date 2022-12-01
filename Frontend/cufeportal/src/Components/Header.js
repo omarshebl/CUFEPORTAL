8669af3d06a858d653a1b91c00b9842c194be8af
shebl007byte <omar.mahmoud001@eng-st.cu.edu.eg>
2022-12-01 09:31:47 +0200

    add: header.js
    React component, header of the website

---
+++ Frontend/cufeportal/src/Components/Header.js
@@ -0,0 +1,9 @@
+// TODO Complete the Header, Link name to root, complete login feature
+export default function Header() {
+    return (
+        <header className="header">
+            <p className="header-text"> cufeportal</p>
+            <button className='login-button'>Login</button>
+        </header>
+    )
+}
