fa1256e158c681d70500ed7c8c05793b38e133df
shebl007byte <omar.mahmoud001@eng-st.cu.edu.eg>
2022-12-01 10:13:04 +0200

    upd: get_database.js
    added database procedures instead of SELECT statements

--- Server/src/Scripts/get_database.js
+++ Server/src/Scripts/get_database.js
@@ -1,10 +1,30 @@
-function get_last_version(connection, callback) {
+const procedures = {
+    classes_from_r_time: "CALL GET_CLASS_FROM_R_TIME(?,?,?);", 
+    classes_from_code: "CALL GET_CLASSES_FROM_CODE(?);",
+    classes_from_id: "CALL GET_CLASSES_FROM_ID(?);",
+    e_room: "CALL GET_E_ROOM_B_T(?,?);",
+    final_from_id: "CALL GET_FINAL_FROM_ID(?);",
+    midterm_from_id: "CALL GET_MIDTERM_FROM_ID(?);",
+    last_version: "CALL GET_LAST_VERSION();",
+    last_class_index: "CALL GET_LAST_CLASS_INDEX();",
+    students_from_class_id: "CALL GET_STUDENTS_FROM_CLASS_ID(?);",
+    students_from_code: "CALL GET_STUDENTS_FROM_CODE(?);",
+    students_from_code_nd: "CALL GET_STUDENTS_FROM_CODE_ND(?);",
+    students_from_r_time: "CALL GET_STUDENTS_FROM_R_TIME(?,?,?);"
+}
+
+function cufeportal(connection, query, callback) {
     return new Promise((resolve, reject) => {
-        connection.execute("SELECT MAX(VERSION_ID) AS VERSION FROM StudentRegClassesVer", function(err, result) {
-            if (err) reject(err);
-            resolve(result[0]['VERSION']);
-        })
+        connection.execute({sql: query.sql, values: query.data}, 
+            function(err, result) {
+                if (err) reject(err);
+                resolve(result[0]);
+            }
+        )
     })
 }
 
-module.exports = {get_last_version};
+module.exports = {
+    cufeportal: cufeportal,
+    procedures: procedures
+}
