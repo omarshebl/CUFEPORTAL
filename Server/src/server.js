08921a9a525718e4d555340690ae7402bb031655
shebl007byte <omar.mahmoud001@eng-st.cu.edu.eg>
2022-12-01 15:53:35 +0200

    upd: server.js
    started working on the server backend to start receiving requests

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,11 +1,32 @@
-const path = require("path")
-const express = require("express")
+const path = require("path");
+const express = require("express");
+const handle_req = require('./handle_req');
 
-const server = express()
-const publicPath = path.join(__dirname, "../public")
+const app = express();
+const publicPath = path.join(__dirname, "../public");
+const viewPath = path.join(__dirname, '../views');
 
-server.get("", (req, res) => {
-    res.send("Hello World!")
+app.set('view engine', 'hbs');
+app.set('views', viewPath);
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+app.use(express.static(publicPath))
+app.get('', (req, res) => {
+    res.render('index', {
+        script_lnk: 'location.js',
+        css_lnk: 'location.css' 
+    });
 })
 
-server.listen(3000)
+app.post('/req', async (req, res) => {
+    const data = await handle_req(req.body);
+    res.json(data);
+});
+
+app.get('*', (req, res) => {
+    res.render('404'); 
+});
+
+app.listen(3000);
